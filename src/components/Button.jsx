21ebf09d09c9b29bd21644c602987afedc0344dd
Jony <jony378892@gmail.com>
2026-02-26 10:49:49 +0600

    Modified Button and Loading Component by making them for interactive

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ export default function Button({ text, handleClick, className }) {
   return (
     <button
       onClick={handleClick}
-      className={`bg-blue-500 text-white rounded-md py-2 px-5 ${className}`}
+      className={`bg-blue-500 text-white rounded-md cursor-pointer py-2 px-5 ${className}`}
     >
       {text}
     </button>
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import { AiOutlineLoading } from "react-icons/ai";
 
 export default function Loading() {
   return (
-    <div className="flex flex-1 items-center justify-center h-96">
+    <div className="flex flex-1 items-center justify-center h-96 pt-36">
       <AiOutlineLoading className="animate-spin size-11" />
     </div>
   );

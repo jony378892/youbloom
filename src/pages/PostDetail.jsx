b0486b9adc7238352d56fbfe541556fb1d9cd8d6
Jony <jony378892@gmail.com>
2026-02-26 10:53:54 +0600

    Added data fetching functionality with loading state

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,3 +1,30 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import Loading from "../components/Loading";
+
 export default function PostDetail() {
+  const [post, setPost] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      setLoading(true);
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${id}`,
+      );
+      const data = await res.json();
+      setPost(data);
+      // console.log(data);
+      setLoading(false);
+    };
+
+    fetchPost();
+  }, [id]);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return <div>This is the detail page</div>;
 }
